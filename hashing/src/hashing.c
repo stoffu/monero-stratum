a45ab8a79b8c08e823ca52812d6583fecc3e8822
stoffu <stoffu@protonmail.ch>
2018-03-12 14:13:44 +0900

    CryptoNight-Lite variant from v2

--- hashing/src/hashing.c
+++ hashing/src/hashing.c
@@ -1,7 +1,8 @@
 #include "crypto/hash-ops.h"
 
 void cryptonight_hash(const char* input, char* output, uint32_t len) {
-    cn_slow_hash(input, len, output, 1);
+    const int variant = input[0] >= 2 ? input[0] - 1 : 0;
+    cn_slow_hash(input, len, output, 1, 1);
 }
 
 void cryptonight_fast_hash(const char* input, char* output, uint32_t len) {
